c4643b0f8f90e5cdfd92a98cde10cbf1ca30190f
Krognol <timgabe@gmail.com>
2017-04-05 23:26:25 +0200

    example

---
+++ examples/cache_message.nim
@@ -0,0 +1,27 @@
+## Has to be compiled with 
+## '-d:ssl' and '--threads:on' flags
+
+import asyncdispatch, discord, tables
+
+var cachedMessages: Table[string, string]
+cachedMessages = initTable[string, string]()
+
+proc messageCreate(s: Session, m: Message) =
+    echo "Message was created!"
+    if m.author.id != s.State.me.id:
+        cachedMessages[m.id] = m.content
+
+proc messageDelete(s: Session, m: MessageDelete) =
+    echo "Message was deleted"
+    if cachedMessages.hasKey(m.id):
+        discard s.SendMessage(m.channel_id, "Message removed: " & cachedMessages[m.id])
+        cachedMessages.del(m.id)
+
+
+
+let s = NewSession("Bot MjM0MDUwNDQ4ODE2OTk2MzYz.C8bxPQ.hGNFiuNIc-6ORpQweZgIYdwTTCU")
+s.messageCreate = messageCreate
+s.messageDelete = messageDelete
+
+asyncCheck s.SessionStart()
+runForever()
