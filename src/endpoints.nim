5d0132fccb781b39219ef011532d078ba3e940e7
Krognol <timgabe@gmail.com>
2017-04-14 21:09:05 +0200

    version

--- src/endpoints.nim
+++ src/endpoints.nim
@@ -3,7 +3,7 @@
 const
     BASE: string = "https://discordapp.com/api"
     GATEWAYVERSION: string = "?v=6&encoding=json"
-    VERSION: string = "1.0"
+    VERSION: string = "1.2"
 
 proc Gateway(): string =
     return BASE & "/gateway"
