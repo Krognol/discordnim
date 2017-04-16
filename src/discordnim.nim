4d340cf548f2717b1f1f5e453247d1d78bce472c
Krognol <timgabe@gmail.com>
2017-04-17 01:27:11 +0200

    documentation

--- src/discordnim.nim
+++ src/discordnim.nim
@@ -1921,6 +1921,10 @@ proc newWebhookParams*(content, username, avatarurl: string = "",
     )
 
 proc messageGuild*(s: Session, m: Message): string =
+    ## Returns the guild id of the guild
+    ## the message was sent in.<br>
+    ## Returns an empty string if it can't find the guild in the cache
+    ## or by requesting it from the API.
     if s.cache.cacheChannels:
         var (chan, exists) = s.cache.getChannel(m.channel_id)
         if exists:
