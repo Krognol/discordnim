761773a2054660c4db4a8db1497c407fa6af6841
Krognol <timgabe@gmail.com>
2017-05-20 02:06:42 +0200

    updated example

--- examples/filesend.nim
+++ examples/filesend.nim
@@ -6,9 +6,7 @@ import asyncdispatch, discord, logging
 proc messageCreate(s: Session, m: MessageCreate) =
     if s.cache.me.id == m.author.id: return
     if m.content == "img":
-        # will not work without editing the httpclient library
-        # refer to https://github.com/nim-lang/Nim/commit/5cf31417a6fcbe5a40bce792652fc05fc6a1cff9
-        discard s.SendFileWithMessage(m.channel_id, "ZWn5Eqt.png", "asdasd")
+        discard s.SendFileWithMessage(m.channel_id, "asdasd.png", "asdasd")
 
 let s = NewSession("Bot <token>")
 s.messageCreate = messageCreate
