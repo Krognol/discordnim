f4778d9507909dd4fa690d928d7a1d2328630cdf
Krognol <timgabe@gmail.com>
2017-06-29 16:37:32 +0200

    updated example

--- examples/cache_message.nim
+++ examples/cache_message.nim
@@ -2,26 +2,24 @@
 ## '-d:ssl' flag
 
 import asyncdispatch, discord, tables 
-proc main() =
-    var cachedMessages: Table[string, string]
-    cachedMessages = initTable[string, string]()
-    
-    let messageCreateProc = proc(s: Session, m: MessageCreate) =
-        echo "Message was created!"
-        if m.author.id != s.cache.me.id:
-            cachedMessages[m.id] = m.content
 
-    let messageDeleteProc = proc(s: Session, m: MessageDelete) =
-        echo "Message was deleted"
-        if cachedMessages.hasKey(m.id):
-            asyncCheck s.SendMessage(m.channel_id, "Message removed: " & cachedMessages[m.id])
-            cachedMessages.del(m.id)
+var cachedMessages: Table[string, string]
+cachedMessages = initTable[string, string]()
+
+let messageCreateProc = proc(s: Session, m: MessageCreate) =
+    echo "Message was created!"
+    if m.author.id != s.cache.me.id:
+        cachedMessages[m.id] = m.content
+ 
+let messageDeleteProc = proc(s: Session, m: MessageDelete) =
+    echo "Message was deleted"
+    if cachedMessages.hasKey(m.id):
+        asyncCheck s.SendMessage(m.channel_id, "Message removed: " & cachedMessages[m.id])
+        cachedMessages.del(m.id)
 
 
-    let s = NewSession("Bot <your bot token>")
-    s.addHandler(EventType.message_create, messageCreateProc)
-    s.addHandler(EventType.message_delete, messageDeleteProc)
+let s = NewSession("Bot <your bot token>")
+s.addHandler(EventType.message_create, messageCreateProc)
+s.addHandler(EventType.message_delete, messageDeleteProc)
 
-    waitFor s.SessionStart()
-
-main()
+waitFor s.SessionStart()
