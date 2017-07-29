b75953a32e29b996313ed86ac6756db8b82cf1db
Krognol <timgabe@gmail.com>
2017-07-29 22:45:42 +0200

    version, edited endpoint procs

--- src/endpoints.nim
+++ src/endpoints.nim
@@ -11,124 +11,124 @@ const
     CDN_CHANNEL_ICONS = CDN_BASE & "/channel-icons"
     
     GATEWAYVERSION = "?v=7&encoding=json"
-    VERSION* = "1.7.0"
+    VERSION* = "1.8.0"
 
-proc gateway*(): string = BASE & "/gateway/bot"
+proc gateway*(): string {.inline.} = BASE & "/gateway/bot"
 
 # CDN endpoints
 
-proc endpointAttachment*(cid, aid, fname: string): string = CDN_ATTACHMENTS & cid & "/" & aid & "/" & fname & ".png"
+proc endpointAttachment*(cid, aid, fname: string): string {.inline.} = CDN_ATTACHMENTS & cid & "/" & aid & "/" & fname & ".png"
 
-proc endpointAvatar*(uid, hash: string): string = CDN_AVATARS & uid & "/" & hash & ".png"
+proc endpointAvatar*(uid, hash: string): string {.inline.} = CDN_AVATARS & uid & "/" & hash & ".png"
 
-proc endpointGuildIcon*(gid, hash: string): string = CDN_ICONS & gid & "/" & hash & ".png"
+proc endpointGuildIcon*(gid, hash: string): string {.inline.} = CDN_ICONS & gid & "/" & hash & ".png"
 
-proc endpointGuildSplash*(gid, hash: string): string = CDN_SPLASHES & gid & "/" & hash & ".png"
+proc endpointGuildSplash*(gid, hash: string): string {.inline.} = CDN_SPLASHES & gid & "/" & hash & ".png"
 
-proc endpointGroupIcon*(cid, hash: string): string = CDN_CHANNEL_ICONS & cid & "/" & hash & ".png"
+proc endpointGroupIcon*(cid, hash: string): string{.inline.} = CDN_CHANNEL_ICONS & cid & "/" & hash & ".png"
 
 # Channel endpoints
 
-proc endpointChannels*(cid : string): string = BASE & "/channels/" & cid
+proc endpointChannels*(cid : string): string {.inline.} = BASE & "/channels/" & cid
 
-proc endpointChannelMessages*(cid : string): string = endpointChannels(cid) & "/messages"
+proc endpointChannelMessages*(cid : string): string {.inline.} = endpointChannels(cid) & "/messages"
 
-proc endpointChannelMessage*(cid, mid : string): string = endpointChannelMessages(cid) & "/" & mid
+proc endpointChannelMessage*(cid, mid : string): string {.inline.} = endpointChannelMessages(cid) & "/" & mid
 
-proc endpointReactions*(cid, mid: string): string = endpointChannelMessage(cid, mid) & "/reactions"
+proc endpointReactions*(cid, mid: string): string{.inline.} = endpointChannelMessage(cid, mid) & "/reactions"
 
-proc endpointOwnReactions*(cid, mid, eid: string): string = endpointReactions(cid, mid) & "/@me"
+proc endpointOwnReactions*(cid, mid, eid: string): string {.inline.} = endpointReactions(cid, mid) & "/@me"
 
-proc endpointMessageReactions*(cid, mid, eid: string): string = endpointReactions(cid, mid) & eid
+proc endpointMessageReactions*(cid, mid, eid: string): string {.inline.} = endpointReactions(cid, mid) & eid
 
-proc endpointMessageUserReaction*(cid, mid, eid, uid: string): string = endpointMessageReactions(cid, mid, eid) & "/" & uid
+proc endpointMessageUserReaction*(cid, mid, eid, uid: string): string {.inline.} = endpointMessageReactions(cid, mid, eid) & "/" & uid
 
-proc endpointBulkDelete*(cid : string): string = endpointChannelMessages(cid) & "/bulk-delete"
+proc endpointBulkDelete*(cid : string): string {.inline.} = endpointChannelMessages(cid) & "/bulk-delete"
 
-proc endpointChannelPermissions*(cid, owid : string): string = endpointChannels(cid) & "/permissions" & owid
+proc endpointChannelPermissions*(cid, owid : string): string {.inline.} = endpointChannels(cid) & "/permissions" & owid
 
-proc endpointChannelInvites*(cid : string): string = endpointChannels(cid) & "/invites"
+proc endpointChannelInvites*(cid : string): string {.inline.} = endpointChannels(cid) & "/invites"
 
-proc endpointTriggerTypingIndicator*(cid : string): string = endpointChannels(cid) & "/typing"
+proc endpointTriggerTypingIndicator*(cid : string): string {.inline.} = endpointChannels(cid) & "/typing"
 
-proc endpointChannelPinnedMessages*(cid : string): string = endpointChannels(cid) & "/pins"
+proc endpointChannelPinnedMessages*(cid : string): string {.inline.} = endpointChannels(cid) & "/pins"
 
-proc endpointPinnedChannelMessage*(cid, mid : string): string = endpointChannelPinnedMessages(cid) & "/" & mid
+proc endpointPinnedChannelMessage*(cid, mid : string): string {.inline.} = endpointChannelPinnedMessages(cid) & "/" & mid
 
-proc endpointGroupDMRecipient*(cid, uid : string): string = endpointChannels(cid) & "/recipients/" & uid
+proc endpointGroupDMRecipient*(cid, uid : string): string {.inline.} = endpointChannels(cid) & "/recipients/" & uid
 
 # Guild endpoints
 
-proc endpointGuilds*(): string = BASE & "/guilds"
+proc endpointGuilds*(): string {.inline.} = BASE & "/guilds"
 
-proc endpointGuild*(gid : string): string = endpointGuilds() & "/" & gid
+proc endpointGuild*(gid : string): string {.inline.} = endpointGuilds() & "/" & gid
 
-proc endpointGuildChannels*(gid : string): string = endpointGuild(gid) & "/channels"
+proc endpointGuildChannels*(gid : string): string {.inline.} = endpointGuild(gid) & "/channels"
 
-proc endpointGuildMembers*(gid : string): string = endpointGuild(gid) & "/members"
+proc endpointGuildMembers*(gid : string): string {.inline.} = endpointGuild(gid) & "/members"
 
-proc endpointGuildMember*(gid, uid : string): string = endpointGuildMembers(gid) & "/" & uid
+proc endpointGuildMember*(gid, uid : string): string {.inline.} = endpointGuildMembers(gid) & "/" & uid
 
-proc endpointEditNick*(gid : string): string = endpointGuildMembers(gid) & "/@me/nick"
+proc endpointEditNick*(gid : string): string {.inline.} = endpointGuildMembers(gid) & "/@me/nick"
 
-proc endpointGuildMemberRoles*(gid, uid, rid : string): string = endpointGuildMember(gid, uid) & "/roles/" & rid
+proc endpointGuildMemberRoles*(gid, uid, rid : string): string {.inline.} = endpointGuildMember(gid, uid) & "/roles/" & rid
 
-proc endpointGuildBans*(gid : string): string = endpointGuild(gid) & "/bans"
+proc endpointGuildBans*(gid : string): string {.inline.} = endpointGuild(gid) & "/bans"
 
-proc endpointGuildBan*(gid, uid : string): string = endpointGuildBans(gid) & "/" & uid
+proc endpointGuildBan*(gid, uid : string): string {.inline.} = endpointGuildBans(gid) & "/" & uid
 
-proc endpointGuildRoles*(gid : string): string = endpointGuild(gid) & "/roles"
+proc endpointGuildRoles*(gid : string): string {.inline.} = endpointGuild(gid) & "/roles"
 
-proc endpointGuildRole*(gid, rid : string): string = endpointGuildRoles(gid) & "/" & rid
+proc endpointGuildRole*(gid, rid : string): string {.inline.} = endpointGuildRoles(gid) & "/" & rid
 
-proc endpointGuildPruneCount*(gid : string): string = endpointGuild(gid) & "/prune"
+proc endpointGuildPruneCount*(gid : string): string {.inline.} = endpointGuild(gid) & "/prune"
 
-proc endpointGuildVoiceRegions*(gid : string): string = endpointGuild(gid) & "/regions"
+proc endpointGuildVoiceRegions*(gid : string): string {.inline.} = endpointGuild(gid) & "/regions"
 
-proc endpointGuildInvites*(gid : string): string = endpointGuild(gid) & "/invites"
+proc endpointGuildInvites*(gid : string): string {.inline.} = endpointGuild(gid) & "/invites"
 
-proc endpointGuildIntegrations*(gid : string): string = endpointGuild(gid) & "/integrations"
+proc endpointGuildIntegrations*(gid : string): string {.inline.} = endpointGuild(gid) & "/integrations"
 
-proc endpointGuildIntegration*(gid, iid : string): string = endpointGuildIntegrations(gid) & "/" & iid
+proc endpointGuildIntegration*(gid, iid : string): string {.inline.} = endpointGuildIntegrations(gid) & "/" & iid
 
-proc endpointSyncGuildIntegration*(gid, iid : string): string = endpointGuildIntegration(gid, iid) & "/sync"
+proc endpointSyncGuildIntegration*(gid, iid : string): string {.inline.} = endpointGuildIntegration(gid, iid) & "/sync"
 
-proc endpointGuildEmbed*(gid : string): string = endpointGuild(gid) & "/embed"
+proc endpointGuildEmbed*(gid : string): string {.inline.} = endpointGuild(gid) & "/embed"
 
-proc endpointGuildAuditLog(gid: string): string = endpointGuild(gid) & "/audit-logs"
+proc endpointGuildAuditLog(gid: string): string {.inline.} = endpointGuild(gid) & "/audit-logs"
 
 # Invite endpoints
 
-proc endpointInvite*(ic : string): string = BASE & "/invites/" & ic
+proc endpointInvite*(ic : string): string {.inline.} = BASE & "/invites/" & ic
 
 # User endpoints
 
-proc endpointCurrentUser*(): string = BASE & "/users/@me"
+proc endpointCurrentUser*(): string {.inline.} = BASE & "/users/@me"
 
-proc endpointUser*(uid : string): string = BASE & "/users/" & uid
+proc endpointUser*(uid : string): string {.inline.} = BASE & "/users/" & uid
 
-proc endpointCurrentUserGuilds*(): string = endpointCurrentUser() & "/guilds"
+proc endpointCurrentUserGuilds*(): string {.inline.} = endpointCurrentUser() & "/guilds"
 
-proc endpointLeaveGuild*(gid : string): string = endpointCurrentUserGuilds() & "/" & gid
+proc endpointLeaveGuild*(gid : string): string {.inline.} = endpointCurrentUserGuilds() & "/" & gid
 
-proc endpointUserDMs*(): string = endpointCurrentUser() & "/channels"
+proc endpointUserDMs*(): string {.inline.} = endpointCurrentUser() & "/channels"
 
-proc endpointDM*(): string = endpointUserDMs()
+proc endpointDM*(): string {.inline.} = endpointUserDMs()
 
-proc endpointUsersConnections*(): string = endpointCurrentUser() & "/connections"
+proc endpointUsersConnections*(): string {.inline.} = endpointCurrentUser() & "/connections"
 
 # Voice endpoint
 
-proc endpointListVoiceRegions*(): string = BASE & "/voice/regions"
+proc endpointListVoiceRegions*(): string {.inline.} = BASE & "/voice/regions"
 
 # Webhook endpoints
 
-proc endpointWebhooks*(cid : string): string = endpointChannels(cid) & "/webhooks"
+proc endpointWebhooks*(cid : string): string {.inline.} = endpointChannels(cid) & "/webhooks"
 
-proc endpointGuildWebhooks*(gid: string): string = endpointGuild(gid) & "/webhooks"
+proc endpointGuildWebhooks*(gid: string): string {.inline.} = endpointGuild(gid) & "/webhooks"
 
-proc endpointWebhook*(wid : string): string = BASE & "/webhooks/" & wid
+proc endpointWebhook*(wid : string): string {.inline.} = BASE & "/webhooks/" & wid
 
-proc endpointWebhookWithToken*(wid, token : string): string = endpointWebhook(wid) & "/" & token
+proc endpointWebhookWithToken*(wid, token : string): string {.inline.} = endpointWebhook(wid) & "/" & token
 
-proc endpointAuth*(): string = BASE & "/auth"
+proc endpointAuth*(): string {.inline.} = BASE & "/auth"
